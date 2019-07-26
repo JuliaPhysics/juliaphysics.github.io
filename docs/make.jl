11d3309178be687fffc811db9a8a76f794b0f37a
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-07-26 23:27:59 +0530

    initialize files

---
+++ docs/make.jl
@@ -0,0 +1,12 @@
+using Documenter
+
+makedocs(
+    pages = [
+        "JuliaPhysics" => [
+            "index.md"
+        ],
+        "Julia Physics ecosystem" => [
+            "ecosystem/index.md",
+        ]
+    ]
+)
